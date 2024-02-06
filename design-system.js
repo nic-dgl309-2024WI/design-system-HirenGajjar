289f9ff6c71eacb5ff3e00030938a24881bf25f0
HirenGajjar <gajjarhiren111@gmail.com>
2024-02-05 21:41:58 -0800

    Add script for hamburger menu icon functionality

--- design-system.js
+++ design-system.js
@@ -1,1 +1,18 @@
-let hamburger = document.querySelector("#hamburger");
+let hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function() {
+    let sidebarMenu = document.querySelector(".sidebar-menu");
+    let wideScreenSidebarMenu = document.querySelector(".wide-screen-sidebar-menu");
+    sidebarMenu.classList.toggle("view");
+    hamburger.classList.toggle("open");
+    wideScreenSidebarMenu.classList.toggle("view");
+};
+
+window.addEventListener('resize', function() {
+    if (window.innerWidth > 600) {
+        document.querySelector('.nav__links').style.display = 'block';
+    } else {
+        document.querySelector('.nav__links').style.display = 'none';
+    }
+});
+
+window.dispatchEvent(new Event('resize'));
