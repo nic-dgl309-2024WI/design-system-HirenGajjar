653825cd73631080daf1c28d4b682e9d4503aabe
HirenGajjar <gajjarhiren111@gmail.com>
2024-03-04 13:36:51 -0800

    Add class name copy functionality

--- design-system.js
+++ design-system.js
@@ -4,3 +4,26 @@ icon.addEventListener('click', () => {
         document.querySelector(".mobile-sidebar").classList.toggle("open");
     }
 });
+
+// copy class
+const copyClass = document.querySelectorAll('.copyClass');
+copyClass.forEach(paragraph => {
+    paragraph.addEventListener('click', function() {
+        const tempInput = document.createElement('input');
+        tempInput.value = this.textContent;
+        document.body.appendChild(tempInput);
+
+        tempInput.select();
+        tempInput.setSelectionRange(0, 99999);
+
+        document.execCommand('copy');
+        document.body.removeChild(tempInput);
+        copyNotification.style.top = `${event.clientY}px`;
+        copyNotification.style.left = `${event.clientX}px`;
+        copyNotification.style.display = 'block';
+        setTimeout(() => {
+            copyNotification.style.display = 'none';
+        }, 500);
+
+    });
+});
