1b20a14875a593fe846df885076378a96b80a2ea
HirenGajjar <gajjarhiren111@gmail.com>
2024-03-22 15:34:14 -0700

    Add navigation to coming soon page

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 let iconHome = document.querySelector("#home-menu-icon");
 iconHome.addEventListener('click', () => {
-
     if (window.innerWidth < 600) {
         document.querySelector(".home-nav__mob-links").classList.toggle("open");
     }
