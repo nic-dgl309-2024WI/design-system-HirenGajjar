7f581499905316ddf7429c593ce46bc0aaa850b1
HirenGajjar <gajjarhiren111@gmail.com>
2024-03-22 15:01:22 -0700

    Add js for mobile menu on home

---
+++ main.js
@@ -0,0 +1,7 @@
+let iconHome = document.querySelector("#home-menu-icon");
+iconHome.addEventListener('click', () => {
+
+    if (window.innerWidth < 600) {
+        document.querySelector(".home-nav__mob-links").classList.toggle("open");
+    }
+});
