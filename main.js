9739b20a7baf4f74146d3b465687078077788e75
HirenGajjar <gajjarhiren111@gmail.com>
2024-03-23 21:09:51 -0700

    Update side bar link order in design system

--- main.js
+++ main.js
@@ -4,3 +4,32 @@ iconHome.addEventListener('click', () => {
         document.querySelector(".home-nav__mob-links").classList.toggle("open");
     }
 });
+
+//Hero image
+const heroDeskImg = document.getElementById('heroDeskImg');
+let ticking = false;
+
+function handleScroll() {
+    const scrollPosition = window.scrollY;
+    const triggerOffset = 150; // Adjust this value as needed
+    let opacity;
+
+    if (scrollPosition > triggerOffset) {
+        // Calculate opacity based on scroll position
+        opacity = 1 - (scrollPosition - triggerOffset) / 200; // Adjust the division factor for faster transition
+        opacity = Math.max(0, opacity); // Ensure opacity is not negative
+    } else {
+        opacity = 1; // Show the image
+    }
+
+    heroDeskImg.style.opacity = opacity;
+
+    ticking = false;
+}
+
+window.addEventListener('scroll', () => {
+    if (!ticking) {
+        window.requestAnimationFrame(handleScroll);
+        ticking = true;
+    }
+});
