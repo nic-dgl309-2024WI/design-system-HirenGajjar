76641cc1cab7b6824a79011469ef63708aec4c0f
HirenGajjar <gajjarhiren111@gmail.com>
2024-03-27 11:47:41 -0700

    Add carousal to desktop view

--- main.js
+++ main.js
@@ -36,6 +36,8 @@ window.addEventListener('scroll', () => {
 
 //carousel
 let slide = document.getElementById('slider');
+
+
 let sliderBtn1 = document.getElementById('btn1');
 let sliderBtn2 = document.getElementById('btn2');
 let sliderBtn3 = document.getElementById('btn3');
@@ -89,3 +91,41 @@ acc.forEach(accordian => {
         accIcon.textContent = accIcon.textContent === '+' ? '-' : '+';
     });
 });
+
+//Desk
+let deskSlider = document.getElementById('desk-slider');
+let deskBtn1 = document.getElementById('desk-btn1');
+let deskBtn2 = document.getElementById('desk-btn2');
+let deskBtn3 = document.getElementById('desk-btn3');
+let deskBtn4 = document.getElementById('desk-btn4');
+window.onload = function() {
+    deskBtn1.classList.add("u-menu-slider-active");
+};
+deskBtn1.onclick = function() {
+    deskSlider.style.transform = "translateX(0px)";
+    deskBtn1.classList.add("u-menu-slider-active");
+    deskBtn2.classList.remove("u-menu-slider-active");
+    deskBtn3.classList.remove("u-menu-slider-active");
+    deskBtn4.classList.remove("u-menu-slider-active");
+};
+deskBtn2.onclick = function() {
+    deskSlider.style.transform = "translateX(-110px)";
+    deskBtn1.classList.remove("u-menu-slider-active");
+    deskBtn2.classList.add("u-menu-slider-active");
+    deskBtn3.classList.remove("u-menu-slider-active");
+    deskBtn4.classList.remove("u-menu-slider-active");
+};
+deskBtn3.onclick = function() {
+    deskSlider.style.transform = "translateX(-300px)";
+    deskBtn1.classList.remove("u-menu-slider-active");
+    deskBtn2.classList.remove("u-menu-slider-active");
+    deskBtn3.classList.add("u-menu-slider-active");
+    deskBtn4.classList.remove("u-menu-slider-active");
+};
+deskBtn4.onclick = function() {
+    deskSlider.style.transform = "translateX(-400px)";
+    deskBtn1.classList.remove("u-menu-slider-active");
+    deskBtn2.classList.remove("u-menu-slider-active");
+    deskBtn3.classList.remove("u-menu-slider-active");
+    deskBtn4.classList.add("u-menu-slider-active");
+};
