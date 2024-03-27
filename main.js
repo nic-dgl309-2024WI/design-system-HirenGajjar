e78d06eaf9822b60c7ba1aec4743cfd489d13e7a
HirenGajjar <gajjarhiren111@gmail.com>
2024-03-27 13:17:52 -0700

    Fix js code for carousel buttons

--- main.js
+++ main.js
@@ -42,8 +42,15 @@ let sliderBtn1 = document.getElementById('btn1');
 let sliderBtn2 = document.getElementById('btn2');
 let sliderBtn3 = document.getElementById('btn3');
 let sliderBtn4 = document.getElementById('btn4');
+let deskSlider = document.getElementById('desk-slider');
+let deskBtn1 = document.getElementById('desk-btn1');
+let deskBtn2 = document.getElementById('desk-btn2');
+let deskBtn3 = document.getElementById('desk-btn3');
+let deskBtn4 = document.getElementById('desk-btn4');
+
 window.onload = function() {
-    sliderBtn1.classList.add("u-menu-slider-active");
+    sliderBtn1.classList.add("u-menu-slider-active")
+    deskBtn1.classList.add("u-menu-slider-desk-active");
 };
 sliderBtn1.onclick = function() {
     slide.style.transform = "translateX(0px)";
@@ -93,39 +100,32 @@ acc.forEach(accordian => {
 });
 
 //Desk
-let deskSlider = document.getElementById('desk-slider');
-let deskBtn1 = document.getElementById('desk-btn1');
-let deskBtn2 = document.getElementById('desk-btn2');
-let deskBtn3 = document.getElementById('desk-btn3');
-let deskBtn4 = document.getElementById('desk-btn4');
-window.onload = function() {
-    deskBtn1.classList.add("u-menu-slider-active");
-};
+
 deskBtn1.onclick = function() {
     deskSlider.style.transform = "translateX(0px)";
-    deskBtn1.classList.add("u-menu-slider-active");
-    deskBtn2.classList.remove("u-menu-slider-active");
-    deskBtn3.classList.remove("u-menu-slider-active");
-    deskBtn4.classList.remove("u-menu-slider-active");
+    deskBtn1.classList.add("u-menu-slider-desk-active");
+    deskBtn2.classList.remove("u-menu-slider-desk-active");
+    deskBtn3.classList.remove("u-menu-slider-desk-active");
+    deskBtn4.classList.remove("u-menu-slider-desk-active");
 };
 deskBtn2.onclick = function() {
     deskSlider.style.transform = "translateX(-110px)";
-    deskBtn1.classList.remove("u-menu-slider-active");
-    deskBtn2.classList.add("u-menu-slider-active");
-    deskBtn3.classList.remove("u-menu-slider-active");
-    deskBtn4.classList.remove("u-menu-slider-active");
+    deskBtn1.classList.remove("u-menu-slider-desk-active");
+    deskBtn2.classList.add("u-menu-slider-desk-active");
+    deskBtn3.classList.remove("u-menu-slider-desk-active");
+    deskBtn4.classList.remove("u-menu-slider-desk-active");
 };
 deskBtn3.onclick = function() {
     deskSlider.style.transform = "translateX(-300px)";
-    deskBtn1.classList.remove("u-menu-slider-active");
-    deskBtn2.classList.remove("u-menu-slider-active");
-    deskBtn3.classList.add("u-menu-slider-active");
-    deskBtn4.classList.remove("u-menu-slider-active");
+    deskBtn1.classList.remove("u-menu-slider-desk-active");
+    deskBtn2.classList.remove("u-menu-slider-desk-active");
+    deskBtn3.classList.add("u-menu-slider-desk-active");
+    deskBtn4.classList.remove("u-menu-slider-desk-active");
 };
 deskBtn4.onclick = function() {
     deskSlider.style.transform = "translateX(-400px)";
-    deskBtn1.classList.remove("u-menu-slider-active");
-    deskBtn2.classList.remove("u-menu-slider-active");
-    deskBtn3.classList.remove("u-menu-slider-active");
-    deskBtn4.classList.add("u-menu-slider-active");
+    deskBtn1.classList.remove("u-menu-slider-desk-active");
+    deskBtn2.classList.remove("u-menu-slider-desk-active");
+    deskBtn3.classList.remove("u-menu-slider-desk-active");
+    deskBtn4.classList.add("u-menu-slider-desk-active");
 };
